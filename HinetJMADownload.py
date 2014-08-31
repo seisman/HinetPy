3c94cc91c8c64fc8f8ecfb1df84c236acc56ad30
Dongdong Tian <seisman.info@gmail.com>
2014-08-31 20:38:18 +0800

    add arrival time and focal mechanism download

---
+++ HinetJMADownload.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author: Dongdong Tian @ USTC
+#
+# Revision History:
+#   2014-08-31  Dongdong Tian   Initial Coding
+#
+
+"""Download arrival time data or focal mechanism catalog from Hi-net.
+
+Usage:
+    HinetJMADownload.py (--measure | --mecha) <yyyymmdd> <span> [--os=OS]
+    HinetJMADownload.py -h
+
+Options:
+    -h --help     Show this help.
+    --measure     Request arrival time data.
+    --mecha       Request focal mechanism catalog.
+    --os=OS       Line break format, choose from DOS or UNIX. [default: DOS]
+
+"""
+import sys
+
+import requests
+from docopt import docopt
+
+# specify user name and password
+user = "xxxxxx"
+passwd = "xxxxxxxxxx"
+
+# base url for continuous waveform data
+base = "http://www.hinet.bosai.go.jp/REGS/JMA/"
+
+
+def download(url, params):
+    d = requests.get(url, params=params, auth=(user, passwd), stream=True)
+    if d.status_code == 401:
+        print("Unauthorized.")
+        sys.exit()
+
+    # file size
+    size = int(d.headers['Content-Length'].strip())
+    # file name
+    disposition = d.headers['Content-Disposition'].strip()
+    fname = disposition.split('filename=')[1].strip('\'"')
+
+    print("Downloading %s ..." % (fname))
+    with open(fname, "wb") as fd:
+        self = 0
+        for chunk in d.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                fd.write(chunk)
+                fd.flush()
+            self += len(chunk)
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+
+    if arguments['--measure']:
+        data = "measure"
+    elif arguments['--mecha']:
+        data = "mecha"
+
+    os = arguments['--os'][0:1]
+
+    url = base + "dlDialogue.php"
+    params = {
+        "data": data,
+        "rtm": arguments['<yyyymmdd>'],
+        "span": arguments['<span>'],
+        "os": arguments['--os'][:1],
+    }
+    download(url, params)
--- README.md
+++ README.md
@@ -2,3 +2,4 @@ This is a collection of scripts for Hi-net data request, download and process.
 
 - `HinetContRequest.py`: Request continuous waveform data from Hi-net.
 - `HinetContDownload.py`: Download continuous waveform datas from Hi-net.
+- `HinetJMADownload.py`: Download arrival time data or focal mechanism catalog.
