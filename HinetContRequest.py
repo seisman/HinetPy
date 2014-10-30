33dcf6dd16f3f3b16148969e96d608dee2f1ad72
Dongdong Tian <seisman.info@gmail.com>
2014-10-30 17:12:16 +0800

    Large Updates
    1. Support request with longer time span
    2. downloading files after requests. no waiting any more.
    3. unzip files and merge minutes to one file.
    4. move options to configure file
    5. add progressbar.

--- HinetContRequest.py
+++ HinetContRequest.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # Author: Dongdong Tian @ USTC
@@ -12,8 +12,11 @@
 #   2014-08-22  Dongdong Tian   Add option for selection of code
 #   2014-08-30  Dongdong Tian   Add option for compressed format.
 #                               Default value (ZIP) is highly recommended.
-#   2014-10-27  Dongdong Tian   Support requests with long time span
-#                               Support downloading after requests
+#   2014-10-27  Dongdong Tian   Support requests with long time span.
+#                               Support downloading after requests.
+#                               Unzip and merge files.
+#                               Move options to configure file.
+#                               Add progressbar.
 #
 
 """Request continuous waveform data from Hi-net.
@@ -46,19 +49,27 @@ Codes of org & net:
 
 """
 
+import os
 import sys
 import time
+import glob
+import shlex
+import zipfile
+import subprocess
 import configparser
 import multiprocessing
 from datetime import date, datetime, timedelta
 
+from clint.textui import progress
 import requests
 from bs4 import BeautifulSoup
 from docopt import docopt
 
+# basic urls
 base = "http://www.hinet.bosai.go.jp/REGS/download/cont/"
 request = base + "cont_request.php"
 status = base + "cont_status.php"
+download = base + "cont_download.php"
 
 # all legal codes
 code_list = ['0101', '0103', '0103A',
@@ -68,12 +79,53 @@ code_list = ['0101', '0103', '0103A',
              '0401',
              '0501',
              '0601',
-             '0701', '0702', '0703', '0705'
+             '0701', '0702', '0703', '0705',
              ]
 
 
+def auth_check(status_code):
+    ''' check authorization '''
+
+    if status_code == 401:
+        print("Unauthorized. Check your username and password!")
+        sys.exit()
+
+
+def date_check(event):
+    ''' check if waveform data are available '''
+
+    start = date(2004, 4, 1)    # start date of avaiable data
+    today = date.today()        # end date of avaiable data
+
+    if event.date() < start or event.date() > today:
+        print("Not within Hi-net service period.")
+        sys.exit()
+
+
+def code_parser(code):
+    ''' parser network code '''
+
+    if code not in code_list:
+        print("%s: Error code for organization and network." % (code))
+        sys.exit()
+    org, net = code[0:2], code[2:]
+
+    return org, net
+
+
+def argument_parser(arguments):
+
+    year = int(arguments['<year>'])
+    month = int(arguments['<month>'])
+    day = int(arguments['<day>'])
+    hour = int(arguments['<hour>'])
+    minute = int(arguments['<min>'])
+
+    return datetime(year, month, day, hour, minute)
+
+
 def cont_request(org, net, event, span, arc):
-    ''' one time request '''
+    ''' request continuous data with limited time span '''
 
     payload = {
         'org1':  org,
@@ -89,10 +141,8 @@ def cont_request(org, net, event, span, arc):
         'LANG':  'en',       # english version of web
         'rn': str(int((datetime.now() - datetime(1970, 1, 1)).total_seconds()))
     }
-    r = requests.post(request, params=payload, auth=(user, passwd), timeout=5)
-    if r.status_code == 401:
-        print("Unauthorized.")
-        sys.exit()
+    r = requests.post(request, params=payload, auth=(user, passwd))
+    auth_check(r.status_code)
 
     s = requests.get(status, auth=(user, passwd))
     soup = BeautifulSoup(s.content)
@@ -105,7 +155,7 @@ def cont_request(org, net, event, span, arc):
         if str(soup.find("tr", class_="bglist1").contents[0].string) == id:
             time.sleep(2)  # still preparing data
         elif str(soup.find("tr", class_="bglist2").contents[0].string) == id:
-            break          # data avaiable
+            break          # data available
         elif str(soup.find("tr", class_="bglist3").contents[0].string) == id:
             print("What's bglist3?")
         elif str(soup.find("tr", class_="bglist4").contents[0].string) == id:
@@ -114,61 +164,66 @@ def cont_request(org, net, event, span, arc):
     return id
 
 
-def date_check(event):
+def cont_download(id):
+    ''' Download continuous waveform data of specified id '''
 
-    start = date(2004, 4, 1)    # start date of avaiable data
-    today = date.today()        # end date of avaiable data
-
-    if event.date() < start or event.date() > today:
-        print("Not within Hi-net service period")
-        sys.exit()
-
-
-def download(url, params):
-    d = requests.get(url, params=params, auth=(user, passwd), stream=True)
-    if d.status_code == 401:
-        print("Unauthorized.")
-        sys.exit()
+    d = requests.get(download, params={"id": id},
+                     auth=(user, passwd), stream=True)
+    auth_check(d.status_code)
 
     # file size
-    size = int(d.headers['Content-Length'].strip())
+    total_length = int(d.headers['Content-Length'].strip())
     # file name
-    disposition = d.headers['Content-Disposition'].strip()
-    fname = disposition.split('filename=')[1].strip('\'"')
+    # disposition = d.headers['Content-Disposition'].strip()
+    # fname = disposition.split('filename=')[1].strip('\'"')
+    fname = "%s.zip" % id   # now use id as filename
 
-    print("Downloading %s ..." % (fname))
     with open(fname, "wb") as fd:
-        self = 0
-        for chunk in d.iter_content(chunk_size=1024):
+        for chunk in progress.bar(d.iter_content(chunk_size=1024),
+                                  label=fname,
+                                  expected_size=(total_length/1024) + 1):
             if chunk:  # filter out keep-alive new chunks
                 fd.write(chunk)
                 fd.flush()
-            self += len(chunk)
+
+    if os.path.getsize(fname) != total_length:
+        print("File %s is not complete!")
 
 
-def cont_download(id):
-    """Download continuous waveform data of specified id"""
+def unzip(zips):
+    """unzip zip filelist"""
 
-    url = base + "cont_download.php"
-    params = {"id": id}
-    download(url, params)
+    for file in zips:
+        zipFile = zipfile.ZipFile(file, "r")
+        for name in zipFile.namelist():
+            zipFile.extract(name)
+
+
+def win32_cat(cnts, cnt_total):
+    """merge WIN32 files to one total WIN32 file"""
+
+    cmd = "%s %s -o %s" % (catwin32, ' '.join(cnts), cnt_total)
+    args = shlex.split(cmd)
+    subprocess.call(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
+def unlink_lists(files):
+    for f in files:
+        os.unlink(f)
 
 
 if __name__ == "__main__":
-
     # User name & passwd
     config = configparser.ConfigParser()
     config.read("Hinet.cfg")
     user = config['Account']['User']
     passwd = config['Account']['Password']
     maxspan = int(config['Cont']['MaxSpan'])
+    catwin32 = config['Tools']['catwin32']
 
     # Code for org & net
     code = config['Cont']['Net']
-    if code not in code_list:
-        print("%s: Error code for organization and network." % (code))
-        sys.exit()
-    org, net = code[0:2], code[2:]
+    org, net = code_parser(code)
 
     # compressed format
     arc = config['Cont']['Format']
@@ -177,26 +232,30 @@ if __name__ == "__main__":
         sys.exit()
 
     arguments = docopt(__doc__)
-    # start time and time span
-    year = int(arguments['<year>'])
-    month = int(arguments['<month>'])
-    day = int(arguments['<day>'])
-    hour = int(arguments['<hour>'])
-    minute = int(arguments['<min>'])
+    event = argument_parser(arguments)
     span = int(arguments['<span>'])
-
-    # check validity of time
-    event = datetime(year, month, day, hour, minute)
     date_check(event)
 
     print("%s ~%s" % (event.strftime("%Y-%m-%d %H:%M"), span))
 
-    ids = []
+    ids, zips = [], []
     while span > 0:
         req_span = min(span, maxspan)
         id = cont_request(org, net, event, req_span, arc)
         ids.append(id)
+        zips.append("%s.zip" % id)
         event += timedelta(minutes=req_span)
         span -= req_span
 
-    multiprocessing.Pool(processes=10).map(cont_download, ids)
+    procs = min(len(ids), multiprocessing.cpu_count())
+    multiprocessing.Pool(processes=procs).map(cont_download, ids)
+
+    # unzip zip files
+    unzip(zips)
+    unlink_lists(zips)
+
+    # merge win32 files
+    cnts = sorted(glob.glob("??????????????????.cnt"))
+    cnt_total = "%s_%d.cnt" % (cnts[0][0:12], len(cnts))
+    win32_cat(cnts, cnt_total)
+    unlink_lists(cnts)
