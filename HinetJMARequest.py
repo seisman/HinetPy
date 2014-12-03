73313a3a16f79423c4d4417b55afd11197e9d12c
Dongdong Tian <seisman.info@gmail.com>
2014-12-03 17:01:26 +0800

    update for hinet v141201

--- HinetJMARequest.py
+++ HinetJMARequest.py
@@ -5,6 +5,7 @@
 #
 # Revision History:
 #   2014-08-31  Dongdong Tian   Initial Coding
+#   2014-12-03  Dongdong Tian   Update to Hinet V141201
 #
 
 """Request arrival time data or focal mechanism catalog from Hi-net.
@@ -29,49 +30,49 @@ from docopt import docopt
 # specify user name and password
 config = configparser.ConfigParser()
 config.read("Hinet.cfg")
-user = config['Account']['User']
-passwd = config['Account']['Password']
+auth = {
+    'auth_un': config['Account']['User'],
+    'auth_pw': config['Account']['Password'],
+    }
 
 # base url for continuous waveform data
-base = "http://www.hinet.bosai.go.jp/REGS/JMA/"
-
-
-def download(url, params):
-    d = requests.get(url, params=params, auth=(user, passwd), stream=True)
-    if d.status_code == 401:
-        print("Unauthorized.")
-        sys.exit()
-
-    # file size
-    size = int(d.headers['Content-Length'].strip())
-    # file name
-    disposition = d.headers['Content-Disposition'].strip()
-    fname = disposition.split('filename=')[1].strip('\'"')
-
-    with open(fname, "wb") as fd:
-        self = 0
-        for chunk in d.iter_content(chunk_size=1024):
-            if chunk:  # filter out keep-alive new chunks
-                fd.write(chunk)
-                fd.flush()
-            self += len(chunk)
+base = "https://hinetwww11.bosai.go.jp/auth/JMA/"
 
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
+    requests.packages.urllib3.disable_warnings()
 
     if arguments['--measure']:
         data = "measure"
     elif arguments['--mecha']:
         data = "mecha"
 
+    rtm = arguments['<yyyymmdd>']
+    span = arguments['<span>']
     os = arguments['--os'][0:1]
 
     url = base + "dlDialogue.php"
     params = {
         "data": data,
-        "rtm": arguments['<yyyymmdd>'],
-        "span": arguments['<span>'],
-        "os": arguments['--os'][:1],
+        "rtm": rtm,
+        "span": span,
+        "os": os,
     }
-    download(url, params)
+
+    d = requests.post(url, params=params, data=auth, verify=False, stream=True)
+    if d.status_code == requests.codes.found:
+        print("Maybe unauthorized. Check your username and password.")
+        sys.exit()
+
+    # file size
+    size = int(d.headers['Content-Length'].strip())
+    # file name
+    print(data, rtm, span)
+    fname = "{}_{}_{}.txt".format(data, rtm, span)
+
+    with open(fname, "wb") as fd:
+        for chunk in d.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                fd.write(chunk)
+                fd.flush()
