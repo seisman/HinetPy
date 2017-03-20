58c323189a3da22c9a56c37d49e6d32e002d1442
Dongdong Tian <seisman.info@gmail.com>
2017-03-21 00:45:30 +0800

    change to local test

---
+++ tests/localtest_channels.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Check if a network has more channels"""
+
+from datetime import datetime, timedelta
+
+from HinetPy import Client
+from HinetPy.header import network
+from HinetPy.win32 import _get_channels
+
+
+username = "test_username"
+password = "test_password"
+client = Client(username, password)
+
+difference = {}
+# always set one day before today as starttime
+starttime = datetime.today() - timedelta(days=1)
+for code in sorted(network.keys()):
+    win32, chfile = client.get_waveform(code, starttime, 1)
+    count = len(_get_channels(chfile))
+
+    if count > network[code].channels:  # more
+        difference[code] = count
+
+    for code in difference.keys():
+        print(code, difference[code])
