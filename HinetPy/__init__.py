66b0a6a5406ce17da3b44e8a5b65d9f2eec9fc69
Dongdong Tian <seisman.info@gmail.com>
2017-07-18 20:05:16 +0800

    release 0.4.2 [ci skip]

--- HinetPy/__init__.py
+++ HinetPy/__init__.py
@@ -18,7 +18,7 @@ Basis usage:
 """
 
 __title__ = 'HinetPy'
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 __author__ = 'Dongdong Tian'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014-2017 Dongdong Tian'
