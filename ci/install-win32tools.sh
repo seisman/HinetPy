45087e4fb61cb85bad33003b19c6127b2610075e
Dongdong Tian <seisman.info@gmail.com>
2017-07-23 09:50:01 +0800

    move win32tools installation to ci script [ci skip]

---
+++ ci/install-win32tools.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+python -c "from HinetPy import Client; Client('${HINET_USERNAME}', '${HINET_PASSWORD}')._get_win32tools()"
+tar -xf win32tools.tar.gz
+cd win32tools && make -j && mkdir bin
+mv catwin32.src/catwin32 win2sac.src/win2sac_32 bin/ && cd ..
+rm win32tools.tar.gz
+
+set +e
