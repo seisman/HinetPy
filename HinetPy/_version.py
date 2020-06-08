cc692bf6a6a6cb65dc2def7867e1be4d1f028230
Dongdong Tian <seisman.info@gmail.com>
2020-06-08 14:25:29 -0400

    Fix documentation version (#32)

--- HinetPy/_version.py
+++ HinetPy/_version.py
@@ -42,7 +42,7 @@ def get_config():
     cfg.VCS = "git"
     cfg.style = "pep440"
     cfg.tag_prefix = ""
-    cfg.parentdir_prefix = "None"
+    cfg.parentdir_prefix = "HinetPy-"
     cfg.versionfile_source = "HinetPy/_version.py"
     cfg.verbose = False
     return cfg
