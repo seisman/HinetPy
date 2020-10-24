faa27eab064c0661aa64eaa4c35e687b9e062a69
Dongdong Tian <seisman.info@gmail.com>
2020-10-24 02:33:47 -0400

    Fix docs Makefile

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,6 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import datetime
 import HinetPy
-import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
