73ca73e8ddd05d40b35533be696926f6455f245c
seisman <seisman@users.noreply.github.com>
2021-03-11 22:16:57 +0000

    deploy: 7b5d3e9687352632c61e205b25e12efa1ccae2be

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.8+13.g8672f6f',
+    VERSION: '0.6.8+14.g7b5d3e9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
