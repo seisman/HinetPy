1eff126c16e70590af71dd54ff9335d37a409e6e
seisman <seisman@users.noreply.github.com>
2021-12-05 14:42:38 +0000

    deploy: 4f885a2e5bd22a8cc76a9fbcc2fd6d87e06d7ed2

--- zh_CN/_static/documentation_options.js
+++ zh_CN/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.9+10.gc1cb909',
+    VERSION: '0.6.9+11.g4f885a2',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
