614253978d0081d0046cb11664e5029955f0409e
seisman <seisman@users.noreply.github.com>
2022-01-01 13:56:44 +0000

    deploy: a3d603e95e28e5ac18cdf1d2200de89e2d4658a8

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.6.10.dev83+g1f09d6b',
+    VERSION: 'v0.6.10.dev84+ga3d603e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- zh_CN/_static/documentation_options.js
+++ zh_CN/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.6.10.dev83+g1f09d6b',
+    VERSION: 'v0.6.10.dev84+ga3d603e',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
