c8aa90a6cddb29d4a1f2d9445bf7a4b3455f9a9d
Dongdong Tian <seisman.info@gmail.com>
2022-02-16 09:52:51 +0800

    Remove unused __commit__ variable

--- HinetPy/__init__.py
+++ HinetPy/__init__.py
@@ -26,4 +26,3 @@ from .header import NETWORK
 __all__ = ["Client", "NETWORK", "win32"]
 # Get semantic version through setuptools-scm
 __version__ = f'v{get_distribution("HinetPy").version}'  # e.g. v0.1.2.dev3+g0ab3cd78
-__commit__ = __version__.split("+g")[-1] if "+g" in __version__ else ""  # 0ab3cd78
