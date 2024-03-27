b5d3ca60f3ff571238ffd75e2f676edab1497684
seisman <seisman@users.noreply.github.com>
2024-03-27 12:19:32 +0000

    deploy: 36486e7f9bec9891305e2d508b8be2fc520a3e31

--- zh_CN/_static/documentation_options.js
+++ zh_CN/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: 'v0.7.2.dev40+g97b0180',
+    VERSION: 'v0.7.2.dev41+g36486e7',
     LANGUAGE: 'zh-CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
